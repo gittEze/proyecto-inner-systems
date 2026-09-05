c26978dad0c39605347e4612433fbdb21c1361a8
Thiago89ml <thiagoagustin2005garcia@gmail.com>
2026-09-05 07:35:44 -0300

    Refactor
    -Confirmación de conexión con la base de datos  por medio de SQLi.
    -Cambios en la cuenta de usuario: cambiando el root por un nuevo usuario.

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,16 +1,19 @@
 <?php
 
 $server = "localhost";
-$user = "root";
-$password = "";
+$user = "Aprendomo";
+$password = "PalaCaballoDiamante";
 $database = "softwaredesarrollo";
 
-// 1. Crear la conexión
+
 $conexion = new mysqli($server, $user, $password, $database);
 
-// Verificar la conexión
+
 if ($conexion->connect_errno) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
+
+
+
 
 ?>
