9b26f880a128cb9ac426918a6f1d94fe9866a775
Ezequiel Albornoz <ezequielalbornozferreira01@gmail.com>
2026-09-09 23:19:08 -0300

    refactor: reorganizar archivos y reutilizar menu y footer
    feat: agregar botones cancelar y limpiar formulario

---
+++ PHP/menu.php
@@ -0,0 +1,19 @@
+<div class="contenedorMain" method="POST">
+    <a class="logo" id="logoMain" href="main.php">
+        <img id="logoMainImg" src="../IMG/Logo.png" alt="Logo de Aprendomo">
+    </a>
+
+    <a class="Serv" href="main.php">Inicio</a>
+    <a class="Serv" href="cursos.php">Cursos</a>
+    <a class="Serv" href="proyectos.php">Proyectos educativos</a>
+    <a class="Serv" href="mentorías.php">Mentorías</a>
+
+    <input id="buscador" type="search" placeholder="Buscar cursos, servicios...">
+
+    <form action="login.php" method="POST">
+         <button class="loginBtn" name="btnLogin" value="login" type="submit">Iniciar sesión</button>
+    </form> 
+    <form action="login.php" method="POST">
+        <button class="loginBtn" name="btnRegister" value="register" type="submit">Registrarse</button>
+    </form>
+</div>
